cdd046b1ced1131ad0aad81fe30e871009c13f01
Ahmed-gubara <ahmed.gubara93@gmail.com>
2021-11-28 23:16:31 +0200

    initial commit

---
+++ DotnetTcp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotnetTcp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
